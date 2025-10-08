459cb1bdc4e8594d28f8ee24feb96bb09abe1119
Fantomrat <ilya64game@gmail.com>
2025-10-08 19:02:21 +0700

    update to 1.21.9

---
+++ src/main/java/dev/sterner/culturaldelights/datagen/DataGenerator.java
@@ -0,0 +1,15 @@
+package dev.sterner.culturaldelights.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DataGenerator implements DataGeneratorEntrypoint {
+
+        @Override
+        public void onInitializeDataGenerator(FabricDataGenerator generator) {
+            FabricDataGenerator.Pack pack = generator.createPack();
+
+
+        }
+
+    }
